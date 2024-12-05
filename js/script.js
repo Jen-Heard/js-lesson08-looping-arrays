f068d1e0ee524f0a3ec1608f15d87e553ba9a9ea
Jen-Heard <jlheard11@gmail.com>
2024-12-05 16:19:15 -0600

    Completed looping exercises

--- js/script.js
+++ js/script.js
@@ -11,3 +11,18 @@ var programmingLanguages = [
   "Rust",
   "C#"
 ];
+console.log(programmingLanguages);
+
+for (var language of programmingLanguages) {
+  console.log(`I want to learn ${language}!`);
+};
+
+programmingLanguages.forEach(function (language, index) {
+  // console.log(`${language} ${index + 1}`);
+  console.log(`${index + 1}. ${language}`.toUpperCase());
+});
+
+var updatedLanguages = programmingLanguages.filter(function (language) {
+  return language.includes("y");
+});
+console.log(updatedLanguages);
